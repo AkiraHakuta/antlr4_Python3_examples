330e1a16c2ebaee9badba8ba7c5b426a421000c5
AkiraHakuta <ahkt9898@gmail.com>
2018-05-03 22:58:10 +0900

    Add files via upload

---
+++ 07_PyEmbInTxt/setup.py
@@ -0,0 +1,21 @@
+# python.exe setup.py 
+import cx_Freeze
+import sys
+
+sys.argv.append('build')
+# python.exe setup.py build
+
+base = None
+includes = []
+include_files = ["gen"]
+excludes = []
+packages = ['antlr4']
+executables = [cx_Freeze.Executable(script="pyEmbInTxt.py",targetName="pyText.exe", base = base),]
+
+cx_Freeze.setup(
+    name = "PyEmbInTxt",
+    options = {"build_exe": 
+        {"build_exe":"pyText/","includes":includes, "include_files": include_files, "excludes": excludes,"packages": packages}},
+    version = "1.0",
+    description = "pyText runs Python in text file",
+    executables = executables)
